52517ce91a6b6d6c1d4d32b19ce395532459bc04
Moin Rauf <147373573+MoinRauf@users.noreply.github.com>
2024-10-19 02:55:11 +0500

    fix Error

--- backend/app.js
+++ backend/app.js
@@ -68,6 +68,11 @@ app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 app.use(cookieParser());
 
+// Default route to check the server
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome to the Resource Hive Backend!");
+});
+
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
 

68e7d581f84bd9d86b2d32df859bb9a1b17048be
izmashafqat07 <izmashafqat148@gmail.com>
2024-11-12 10:55:35 +0500

    feat:setting up react hook form

--- frontend/src/components/custom/textField.jsx
+++ frontend/src/components/custom/textField.jsx
@@ -1,26 +1,34 @@
+import { forwardRef } from "react";
 import { Field, Input, Label } from "@headlessui/react";
 import { cn } from "@/utils";
 
-export default function TextField({ label, placeholder, onChange, error }) {
-  return (
-    <div className="w-full ">
-      <Field className={cn("flex flex-col")}>
-        <Label className="text-sm/6 font-semibold text-slate-900">
-          {label}
-        </Label>
+const TextField = forwardRef(
+  ({ label, placeholder, onChange, error, onBlur, value, onKeyDown }, ref) => {
+    return (
+      <div className="w-full ">
+        <Field className={cn("flex flex-col")}>
+          <Label className="text-sm/6 font-semibold text-slate-900">
+            {label}
+          </Label>
 
-        <Input
-          onchange={onChange}
-          placeholder={placeholder}
-          className={cn(
-            "mt-2 block w-full rounded-md border border-slate-300 bg-white py-1.5 px-3 text-sm/6 text-slate-900",
-            " data-[focus]:outline-2 data-[focus]:-outline-offset-2   focus:outline-none focus:ring-2 focus:ring-slate-600  "
-          )}
-        />
-      </Field>
-      <div className=" min-h-4 flex items-center">
-        {error && <p className="text-xs text-red-500">{error}</p>}
+          <Input
+            onKeyDown={onKeyDown}
+            ref={ref}
+            value={value}
+            onBlur={onBlur}
+            onChange={onChange}
+            placeholder={placeholder}
+            className={cn(
+              "mt-2 block w-full rounded-md border border-slate-300 bg-white py-1.5 px-3 text-sm/6 text-slate-900",
+              " data-[focus]:outline-2 data-[focus]:-outline-offset-2   focus:outline-none focus:ring-2 focus:ring-slate-600  "
+            )}
+          />
+        </Field>
+        <div className=" min-h-4 flex items-center">
+          {error && <p className="text-xs text-red-500">{error}</p>}
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
+);
+export default TextField;
--- frontend/src/components/form/loginForm.jsx
+++ frontend/src/components/form/loginForm.jsx
@@ -1,24 +1,87 @@
+import { useState } from "react";
 import { TextField, PasswordField, Button } from "@/components";
 import { Link } from "react-router-dom";
+import { useForm, Controller } from "react-hook-form";
 
 export default function LoginForm() {
+  const [disabled, setDisabled] = useState(false);
+  const { control, handleSubmit, setFocus } = useForm({
+    disabled,
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(data) {
+    setDisabled(true);
+
+    setTimeout(() => {
+      setDisabled(false);
+      console.log("🚀 ~ onSubmit ~ data:", data);
+    }, 4000);
+  }
+
+  function handleKeyDown(e, fieldName) {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      if (fieldName === "submit") {
+        handleSubmit(onSubmit)(); // Submit the form on Enter
+      } else {
+        setFocus(fieldName); // Focus the password field on Enter
+      }
+    }
+  }
   return (
     <div className=" lg:w-1/4 py-4 xl:w-1/4 md:w-1/2 sm:w-1/2 px-4 w-full sm:shadow-[7px_-4px_60px_5px_rgba(72,_66,_66,_0.1)]">
       <h1 className="text-2xl font-bold text-center">Login</h1>
-      <TextField label="Email" placeholder="Enter your email" />
-      <PasswordField label="Password" placeholder="Enter your password" />
-      <div>
-        <Button
-          buttonText={"Login"}
-          buttonType={"submit"}
-          className="mt-3 bg-black  text-sm  w-full "
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          control={control}
+          name="email"
+          render={({ field }) => (
+            <TextField
+              ref={field.ref}
+              value={field.value}
+              onBlur={field.onBlur}
+              onChange={field.onChange}
+              label="Email"
+              placeholder="Enter your email"
+              onKeyDown={(e) => handleKeyDown(e, "password")}
+            />
+          )}
         />
-      </div>
-      <div className="flex flex-col">
-        <Link className="text-xs underline self-center mt-2" to="/sign_up">
-          Create an account
-        </Link>
-      </div>
+
+        <Controller
+          control={control}
+          name="password"
+          render={({ field }) => (
+            <PasswordField
+              ref={field.ref}
+              value={field.value}
+              onBlur={field.onBlur}
+              onChange={field.onChange}
+              label="Password"
+              placeholder="Enter your password"
+              onKeyDown={(e) => handleKeyDown(e, "submit")}
+            />
+          )}
+        />
+
+        <div>
+          <Button
+            disabled={disabled}
+            buttonText={"Login"}
+            buttonType={"submit"}
+            className="mt-3 bg-black  text-sm  w-full "
+          />
+        </div>
+        <div className="flex flex-col">
+          <Link className="text-xs underline self-center mt-2" to="/sign_up">
+            Create an account
+          </Link>
+        </div>
+      </form>
     </div>
   );
 }

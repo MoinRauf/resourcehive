646f15eac6a41b97df747345b8d43d554141aa1f
Moin Rauf <147373573+MoinRauf@users.noreply.github.com>
2024-11-19 22:47:48 +0500

    CHECKING
    CHECKING

--- frontend/src/pages/landingPage.jsx
+++ frontend/src/pages/landingPage.jsx
@@ -3,8 +3,8 @@ export default function LandingPage() {
   return (
     <>
       <div className="flex gap-4">
-        <Link to="/login">Login</Link>
-        <Link to="/sign_up">Signup</Link>
+        <Link to="/login">Log-in</Link>
+        <Link to="/sign_up">Sign-up</Link>
       </div>
     </>
   );

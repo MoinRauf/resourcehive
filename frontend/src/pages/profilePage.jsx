92252902127e74262d66301004dfbd6d80cb1698
Moin Rauf <147373573+MoinRauf@users.noreply.github.com>
2024-11-19 22:44:43 +0500

    TEST
    TEST

--- frontend/src/pages/profilePage.jsx
+++ frontend/src/pages/profilePage.jsx
@@ -2,5 +2,5 @@ import { useSidebar } from "@/hooks";
 
 export default function ProfilePage() {
   const { toggleSidebarOpen } = useSidebar();
-  return <button onClick={toggleSidebarOpen}>open sidebar</button>;
+  return <button onClick={toggleSidebarOpen}>open Sidebar</button>;
 }
